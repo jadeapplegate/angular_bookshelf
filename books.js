e8e7336d929933ff5fa5efc4229b505a8e3b5bac
Jade <jade.applegate@gmail.com>
2014-06-03 17:56:39 -0700

    delete now working

--- books.js
+++ books.js
@@ -1,8 +1,14 @@
 var BooksCtrl = function($scope){
-  $scope.books = [];
-  
-  $scope.addBook = function(){
-    $scope.books.push($scope.newBook);
-    $scope.newBook = {};
-  };
+	$scope.books = [];
+
+	$scope.addBook = function(){
+	  $scope.books.push($scope.newBook);
+	  $scope.newBook = {};
+	};
+
+	  $scope.deleteBook = function(newBook){
+	var index =       $scope.books.indexOf(newBook);
+	$scope.books.splice(index, 1);
+	};
+	
 };
